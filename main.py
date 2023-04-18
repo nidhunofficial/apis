d1a5d112275335f0c237dcf7f71cd7456fcf3ba9
nidhunofficial <129491447+nidhunofficial@users.noreply.github.com>
2023-04-18 09:42:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-# main.py
-
+# -*- coding: utf-8 -*-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import json
+import builtins
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+origins = [
+
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+class model_input(BaseModel):
+    Code : str  
+          
+
+@app.post('/code_prediction')
+def code_predd(input_parameters : model_input):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    
+    code = input_dictionary['Code']
+    
+    code.replace("/r", "")
+    from guesslang import Guess
+
+    guess = Guess()
+    
+    language = guess.language_name(code)
+    
+    return language
+    
